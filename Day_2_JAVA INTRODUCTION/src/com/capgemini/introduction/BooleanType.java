27ea2882680d025a8380f2f91032387aa9c0c5d8
Tushar <Tushar@DESKTOP-TLP5ORB>
2022-08-03 00:13:27 +0530

    Second commit

---
+++ Day_2_JAVA INTRODUCTION/src/com/capgemini/introduction/BooleanType.java
@@ -0,0 +1,16 @@
+package com.capgemini.introduction;
+
+import java.util.Scanner;
+
+public class BooleanType {
+
+	public static void main(String[] args) {
+		Scanner b = new Scanner(System.in);
+		System.out.println("Enter a value : ");
+		boolean a = b.nextBoolean();
+		System.out.println(a);
+		
+
+	}
+
+}
---
+++ Day_2_JAVA INTRODUCTION/src/com/capgemini/introduction/StringTypeProgram.java
@@ -0,0 +1,15 @@
+package com.capgemini.introduction;
+
+import java.util.Scanner;
+
+public class StringTypeProgram {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Give input :");
+		String a = sc.nextLine();
+		System.out.println(a +" " + a.getClass( ));
+
+	}
+
+}
