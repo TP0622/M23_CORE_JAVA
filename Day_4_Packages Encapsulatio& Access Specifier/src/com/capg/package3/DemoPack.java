ffae943f4dc589dffa93ca132cffa0fa74821e92
Tushar <Tushar@DESKTOP-TLP5ORB>
2022-08-06 23:02:31 +0530

    Second Commit

---
+++ Day_4_Packages Encapsulatio& Access Specifier/src/com/capg/package3/DemoPack.java
@@ -0,0 +1,11 @@
+package com.capg.package3;
+
+public class DemoPack 
+{
+	public void accept()
+	{
+		System.out.println("Welcome to Java FullStack");
+	}
+	
+
+}
---
+++ Day_4_Packages Encapsulatio& Access Specifier/src/com/capg/package4/DemoTest.java
@@ -0,0 +1,13 @@
+package com.capg.package4;
+
+import com.capg.package3.*;//another method to access method accept();
+
+public class DemoTest {
+
+	public static void main(String[] args) {
+		DemoPack obj=new DemoPack();
+		obj.accept();
+
+	}
+
+}
