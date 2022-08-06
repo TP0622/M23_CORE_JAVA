7620880c8c858511ae53a074efd6e3468d736a7e
Tushar <Tushar@DESKTOP-TLP5ORB>
2022-08-06 22:50:26 +0530

    First Commit

---
+++ Day_4_Packages Encapsulatio& Access Specifier/src/com/capg/package1/Package.java
@@ -0,0 +1,12 @@
+package com.capg.package1;
+
+public class Package 
+{
+	public int roll_no;
+	public String name;
+	public void display(int roll_no,String name)
+	{
+		System.out.println("Roll no is :" + roll_no + " & name is " + name);
+	}
+}
+
---
+++ Day_4_Packages Encapsulatio& Access Specifier/src/com/capg/packages2/DemoimportPackage.java
@@ -0,0 +1,11 @@
+package com.capg.packages2;
+import com.capg.package1.Package;
+public class DemoimportPackage 
+{
+	public static void main(String [] args)
+	{
+		Package p=new Package();
+		p.display(62, "Tushar");
+	}
+
+}
